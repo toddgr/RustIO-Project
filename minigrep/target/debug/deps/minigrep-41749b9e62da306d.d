5db6c744e8052d3bd65f50a587b5006ea7873dde
Grace Todd <toddgr@oregonstate.edu>
2022-10-07 20:59:28 -0700

    leveloping library's functionality with TDD

---
+++ minigrep/target/debug/deps/minigrep-41749b9e62da306d.d
@@ -0,0 +1,5 @@
+C:\Users\Grace\Desktop\RustIO-Project\minigrep\target\debug\deps\minigrep-41749b9e62da306d.exe: src\lib.rs
+
+C:\Users\Grace\Desktop\RustIO-Project\minigrep\target\debug\deps\minigrep-41749b9e62da306d.d: src\lib.rs
+
+src\lib.rs:
---
+++ minigrep/target/debug/deps/minigrep-bed007b557971686.d
@@ -0,0 +1,5 @@
+C:\Users\Grace\Desktop\RustIO-Project\minigrep\target\debug\deps\minigrep-bed007b557971686.exe: src\main.rs
+
+C:\Users\Grace\Desktop\RustIO-Project\minigrep\target\debug\deps\minigrep-bed007b557971686.d: src\main.rs
+
+src\main.rs:
